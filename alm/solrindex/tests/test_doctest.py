317d65c7c17ae15936548d33ec5da19132d87afd
David Glick <david@glicksoftware.com>
2015-12-04 14:13:31 -0800

    fix

--- alm/solrindex/tests/test_doctest.py
+++ alm/solrindex/tests/test_doctest.py
@@ -1,15 +1,13 @@
-# Disabled until we either mock solr or start it in our test setup.
-
-# from alm.solrindex.tests import base
-# from Testing import ZopeTestCase as ztc
-# import unittest
-# import doctest
+from alm.solrindex.tests import base
+from Testing import ZopeTestCase as ztc
+import unittest
+import doctest
 
 
-# def test_suite():
-#     return unittest.TestSuite([
-#         ztc.FunctionalDocFileSuite(
-#             'zmi.txt',
-#             test_class=base.FunctionalTestCase,
-#             optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
-#         ])
+def test_suite():
+    return unittest.TestSuite([])
+        # ztc.FunctionalDocFileSuite(
+        #     'zmi.txt',
+        #     test_class=base.FunctionalTestCase,
+        #     optionflags=doctest.NORMALIZE_WHITESPACE | doctest.ELLIPSIS),
+        # ])
