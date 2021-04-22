dee1c4ee6cce8a164c78b89c69a6f8ba715fd4ea
Silvio Tomatis <silviot@gmail.com>
2021-04-22 12:54:09 +0200

    Amend test methods: adapted.node.toprettyxml(' ', encoding='utf-8') → adapted.node.toprettyxml(' ')

--- alm/solrindex/tests/test_exportimport.py
+++ alm/solrindex/tests/test_exportimport.py
@@ -1,6 +1,10 @@
+from Products.GenericSetup.interfaces import INode
 from Products.GenericSetup.testing import NodeAdapterTestCase
+from Products.GenericSetup.testing import DummySetupEnviron
 from Products.GenericSetup.testing import ExportImportZCMLLayer
+from xml.dom.minidom import parseString
 from Zope2.App import zcml
+from zope.component import getMultiAdapter
 import unittest
 
 _SOLR_URI = 'http://localhost:8988/solr'
@@ -35,6 +39,38 @@ class SolrIndexNodeAdapterTests(NodeAdapterTestCase, unittest.TestCase):
         return SolrIndexNodeAdapter
 
     def setUp(self):
+        import alm.solrindex
         from alm.solrindex.index import SolrIndex
+        # Needed to make tests work on Plone 5.2
+        # Might not be the right approach: I'm no expert
+        ExportImportZCMLLayer.setUp()
+        zcml.load_config('configure.zcml', alm.solrindex)
+
         self._obj = SolrIndex('Solr', _SOLR_URI)
         self._XML = _SOLRINDEX_XML
+
+    def test_node_get(self):
+        self._populate(self._obj)
+        context = DummySetupEnviron()
+        adapted = getMultiAdapter((self._obj, context), INode)
+        self.assertEqual(adapted.node.toprettyxml(" "), self._XML)
+
+    def test_node_set(self):
+        context = DummySetupEnviron()
+        adapted = getMultiAdapter((self._obj, context), INode)
+        adapted.node = parseString(self._XML).documentElement
+        self._verifyImport(self._obj)
+        self.assertEqual(adapted.node.toprettyxml(" "), self._XML)
+
+        # now in update mode
+        context._should_purge = False
+        adapted = getMultiAdapter((self._obj, context), INode)
+        adapted.node = parseString(self._XML).documentElement
+        self._verifyImport(self._obj)
+        self.assertEqual(adapted.node.toprettyxml(" "), self._XML)
+
+        # and again in update mode
+        adapted = getMultiAdapter((self._obj, context), INode)
+        adapted.node = parseString(self._XML).documentElement
+        self._verifyImport(self._obj)
+        self.assertEqual(adapted.node.toprettyxml(" "), self._XML)
